a96db42cd67bb21503e36d9df2fa35e7dab7d5a0
Bryan Tafel <bryantafel@Bryans-MacBook-Pro.local>
2017-11-10 18:51:35 -0500

    Change package name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "ground:db",
+  name: "btafel:db",
   version: "2.0.0-rc.7",
   summary: "Ground Meteor.Collections offline",
   git: "https://github.com/GroundMeteor/db.git"
